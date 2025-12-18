eef1efaa5efc0d508f86873a3dfa35b5f0934957
ksizos <kseniaizosimova04@gmail.com>
2025-12-18 22:40:12 +0500

    add progressbar in topics

--- app/src/main/java/com/example/englishlearningapp/ui/topics/TopicsAdapter.kt
+++ app/src/main/java/com/example/englishlearningapp/ui/topics/TopicsAdapter.kt
@@ -6,6 +6,7 @@ import android.widget.TextView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 import com.example.englishlearningapp.R
 import com.example.englishlearningapp.data.model.TopicItem
 
@@ -14,28 +15,34 @@ class TopicAdapter(
     private val onClick: (TopicItem) -> Unit
 ) : RecyclerView.Adapter<TopicAdapter.TopicViewHolder>() {
 
-    inner class TopicViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val icon: TextView = view.findViewById(R.id.iconTopic) // новый TextView для иконки
-        val title: TextView = view.findViewById(R.id.title)
-        val progress: TextView = view.findViewById(R.id.timeAgo)
-    }
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TopicViewHolder {
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.item_topics, parent, false)
         return TopicViewHolder(view)
     }
 
+    inner class TopicViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val icon: TextView = view.findViewById(R.id.iconTopic)
+        val title: TextView = view.findViewById(R.id.title)
+        val progress: TextView = view.findViewById(R.id.timeAgo)
+        val progressBar: ProgressBar = view.findViewById(R.id.progressBar) // <- прогрессбар
+    }
+
     override fun onBindViewHolder(holder: TopicViewHolder, position: Int) {
         val item = topics[position]
-        holder.icon.text = item.iconTopic   // отображаем иконку
+        holder.icon.text = item.iconTopic
         holder.title.text = item.title
         holder.progress.text = "${item.learnedWords} / ${item.totalWords}"
+
+        // Установка прогресса
+        holder.progressBar.max = item.totalWords
+        holder.progressBar.progress = item.learnedWords
 
         holder.itemView.setOnClickListener {
             onClick(item)
         }
     }
+
 
     override fun getItemCount() = topics.size
 }
